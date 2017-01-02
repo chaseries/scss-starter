31c580d0c9cff45b934bfe0d15d1b0cdc2e3c338
chaseries <hello@chaseries.com>
2017-01-02 00:43:04 -0500

    Add ExtractText plugin, remove cruft

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,48 @@
+// webpack.config.js
+
 var path = require("path");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+var sassLoader = ExtractTextPlugin.extract('style-loader','css-loader?sourceMap&importLoaders=1!sass-loader?sourceMap');
 
 module.exports = {
-  entry: "./src/main.js",
-  output: {
-    path: path.resolve(__dirname + "/build"),
-    filename: "[name].js"
-  },
-  module: {
-    loaders: [
-      {
-        test: /\.(css|scss)$/,
+    entry: './src/main.js',
+    output: {
+        path: './build/',        
+        filename: 'main.js'
+    },
+    devTool: "source-map",
+    module: {
         loaders: [
-          "style-loader",
-          "css-loader",
-          "sass-loader"
+            { test: /\.scss$/, 
+              loader: sassLoader
+            }
         ]
-      },
-      {
-        test: /\.elm$/,
-        exclude: [/elm-stuff/, /node-modules/],
-        loader: "elm-webpack"
-      }
-    ],
-    noParse: /\.elm$/,
-  }
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css", { allChunks: true })
+    ]
 }
+
+// var path = require("path");
+// var Extract = require("extract-text-webpack-plugin");
+
+// module.exports = {
+  // entry: "./src/main.js",
+  // output: {
+    // path: path.resolve(__dirname + "/build"),
+    // filename: "[name].js"
+  // },
+  // devtool: "source-map",
+  // module: {
+    // loaders: [
+      // {
+        // test: /\.(css|scss)$/,
+        // loaders: [
+          // "style-loader",
+          // "css-loader?sourceMap",
+          // "sass-loader?sourceMap"
+        // ]
+      // }
+    // ]
+  // }
+// }
